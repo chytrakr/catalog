33af3cccae93bf5d58b091869d27960293ebc0bd
Chytra K R <34940260+chytrakr@users.noreply.github.com>
2020-01-27 16:52:28 +0530

    Update settings.js

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,3 +1,3 @@
-uri = "mongodb+srv://chytrakr:manja153@cluster0-nxcni.mongodb.net/test?retryWrites=true";
+uri = "mongodb+srv://<user_name>:<password>@cluster0-nxcni.mongodb.net/test?retryWrites=true";
 
 module.exports = uri
